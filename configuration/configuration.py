fdae8272240da6dea6a01c8f9a6e491e3ff5f034
alessandroblaco <alessandro.blaco@gmail.com>
2025-11-30 23:25:50 +0100

    add topology_views plugin

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -262,13 +262,16 @@ if 'PAGINATE_COUNT' in environ:
     PAGINATE_COUNT = _environ_get_and_map('PAGINATE_COUNT', None, _AS_INT)
 
 # # Enable installed plugins. Add the name of each plugin to the list.
-# PLUGINS = []
+PLUGINS = ["netbox_topology_views"]
 
 # # Plugins configuration settings. These settings are used by various plugins that the user may have installed.
 # # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
-# PLUGINS_CONFIG = {
-# }
-
+PLUGINS_CONFIG = {
+    'netbox_topology_views': {
+        'allow_coordinates_saving': True,
+        'always_save_coordinates': True
+    }
+}
 # When determining the primary IP address for a device, IPv6 is preferred over IPv4 by default. Set this to True to
 # prefer IPv4 instead.
 if 'PREFER_IPV4' in environ:
